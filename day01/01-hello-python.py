32c9033649c16e13e1ca8d553b48d2d115dc5d6a
apt-get-install <ekr7777777@gmail.com>
2020-05-30 10:36:35 +0000

    day-01

--- day01/01-hello-python.py
+++ day01/01-hello-python.py
@@ -1,1 +1,11 @@
-print("Hello, World")
+"""day01-변수
+타입을 지정할 필요가 없다.
+ - 언어에 따라서, 사용할 변수의 타입(문자열, 숫자, 리스트 등)을 명시해야 한다.
+   ex) 자바 -> String name = "최수호"
+"""
+
+name = "최수호"                   # 문자형 변수
+age = 30                         # 숫자형 변수
+hobbies = ["excercise", "game"]  # 리스트 변수
+
+print(hobbies)
